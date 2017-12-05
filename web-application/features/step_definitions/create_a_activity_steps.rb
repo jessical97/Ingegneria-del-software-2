bb4fd6fec0baf4329c052e3166cdd901c10a6c14
Matteo Bortolon <bortolonmatteo@Bortolons-MacBook-Pro.local>
2017-12-05 19:03:07 +0100

    Resolve code style problem

--- web-application/features/step_definitions/create_a_activity_steps.rb
+++ web-application/features/step_definitions/create_a_activity_steps.rb
@@ -1,19 +1,18 @@
 Then('I must find activity in list') do
-    visit '/activities'
-    table = find('table')
-    table_rows = table.all('tr')
-    found = false
-    table_rows.each do |row|
-      contain_all = true
-      @insert.each_value do |value|
-        inside = row.has_text?(value)
-        if inside == false
-          contain_all = false
-          break
-        end
+  visit '/activities'
+  table = find('table')
+  table_rows = table.all('tr')
+  found = false
+  table_rows.each do |row|
+    contain_all = true
+    @insert.each_value do |value|
+      inside = row.has_text?(value)
+      if inside == false
+        contain_all = false
+        break
       end
-      found = true if contain_all == true
     end
-    expect(found).to eq(true)
+    found = true if contain_all == true
   end
-  
+  expect(found).to eq(true)
+end
